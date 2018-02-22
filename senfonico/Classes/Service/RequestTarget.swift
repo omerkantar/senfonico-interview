85881aa0ce5208b3775c0b047afb7e3f146c98af
omer <iomerkantar@gmail.com>
2018-02-22 20:21:26 +0300

    init: proje dogdu 👶🏿

---
+++ senfonico/Classes/Service/RequestTarget.swift
@@ -0,0 +1,34 @@
+//
+//  RequestTarget.swift
+//  senfonico
+//
+//  Created by omer kantar on 22.02.2018.
+//  Copyright © 2018 senfonico-interview. All rights reserved.
+//
+
+import UIKit
+
+fileprivate let k
+enum RequestTarget {
+    
+}
+
+
+
+extension RequestTarget {
+    
+    var basePath: String {
+        return ""
+    }
+    
+    var url: URL {
+        
+        if let url = URL(string: )
+        return URL(string: "www.google.com")!
+    }
+    
+    var path: String {
+        
+    }
+    
+}
---
+++ senfonico/Classes/Service/ServiceManager.swift
@@ -0,0 +1,16 @@
+//
+//  ServiceManager.swift
+//  senfonico
+//
+//  Created by omer kantar on 22.02.2018.
+//  Copyright © 2018 senfonico-interview. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Eger external library kullanabilseydim Network layer icin Moya/Alamofire frameworkleri kullanacaktim
+//
+
+class ServiceManager: NSObject {
+
+}
